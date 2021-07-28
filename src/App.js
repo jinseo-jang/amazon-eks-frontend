dfcb81f66cb100e29f1b29e2a0c65dfda304f1ca
jinseo-jang <jinseo.jang@gmail.com>
2021-07-28 17:43:47 +0900

    new2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog - new1
+            EKS DEMO Blog - new2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
